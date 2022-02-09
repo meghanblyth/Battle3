861c1e577fa4ea9ff2567a8a9d9158735bf8c817
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-02-09 17:38:59 +0000

    first commit

---
+++ spec/features/player_entering_name_spec.rb
@@ -0,0 +1,9 @@
+feature 'Testing name entering' do
+  scenario 'Allow players to enter their name' do
+    visit('/')
+    fill_in :player_1_name, with: "Ali"
+    fill_in :player_2_name, with: "Trushna"
+    click_button "Submit"
+    expect(page).to have_content 'Ali vs. Trushna'
+  end
+end
